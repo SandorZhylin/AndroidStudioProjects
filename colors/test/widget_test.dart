ebc10ac0bc8633668fe2c25f1c27b0d838b2f1b3
Aleksandr Zhylin <a.zhylin1987@gmail.com>
2021-02-23 21:38:06 +0200

    Final version of Colors app.

--- colors/test/widget_test.dart
+++ colors/test/widget_test.dart
@@ -7,12 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:sample_flutter_app/main.dart';
+
+import 'package:colors/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(WidgetsTraining());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
--- sample_flutter_app/lib/main.dart
+++ sample_flutter_app/lib/main.dart
@@ -5,44 +5,42 @@ void main() {
 }
 
 class WidgetsTraining extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Widgets Training',
       home: Scaffold(
         backgroundColor: (Colors.grey),
-        appBar: AppBar(
-        title: Text('This is a widgets test')
-        ),
+        appBar: AppBar(title: Text('This is a widgets test')),
         body: Center(
           child: Container(
-            width: 200,
-            height: 200,
-            color: (Colors.red),
-            child: Column(
+              width: 200,
+              height: 200,
+              color: (Colors.red),
+              child: Column(
                 children: [
-                    Text ('SATOR', style: TextStyle(
-                      color: Colors.blue
-                    ),),
-                    Text ('EREPO', style: TextStyle(
-                        color: Colors.yellow
-                    ),),
-                    Text ('OPERA', style: TextStyle(
-                        color: Colors.green
-                    ),),
-                    Text ('ROTAS', style: TextStyle(
-                    color: Colors.white
-                    ),),
-                  ],
-            )
-          ),
+                  Text(
+                    'SATOR',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  Text(
+                    'EREPO',
+                    style: TextStyle(color: Colors.yellow),
+                  ),
+                  Text(
+                    'OPERA',
+                    style: TextStyle(color: Colors.green),
+                  ),
+                  Text(
+                    'ROTAS',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              )),
         ),
-          bottomSheet: Row(
-          children: [
-            Text ('TENET')
-        ],
-      ),
+        bottomSheet: Row(
+          children: [Text('TENET')],
+        ),
       ),
     );
   }
